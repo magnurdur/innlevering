ec85b3a805c23cfa2071a1b88bf3fc2d1f57174b
magnurdur <trevlandmagnus@gmail.com>
2025-10-22 16:36:44 +0200

    ff

--- obligatorisk/2.vis-alle-klasser.php
+++ obligatorisk/2.vis-alle-klasser.php
@@ -1,1 +1,34 @@
+<?php  
+/* vis-alle-klasser */
+/*
+/*  Programmet viser alle registrerte klasser i databasen
+*/
+?>
 
+<h3>Alle registrerte klasser</h3>
+
+<?php
+include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
+
+$sqlSetning = "SELECT * FROM klasse ORDER BY klassekode;";
+$sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig &aring; hente data fra databasen.");
+
+$antallRader = mysqli_num_rows($sqlResultat);
+
+if ($antallRader == 0) {
+    print("Det finnes ingen registrerte klasser i databasen.");
+} else {
+    print("<table border='1' cellspacing='0' cellpadding='5'>");
+    print("<tr><th>Klassekode</th><th>Klassenavn</th><th>Studiumkode</th></tr>");
+
+    while ($rad = mysqli_fetch_array($sqlResultat)) {
+        $klassekode = $rad["klassekode"];
+        $klassenavn = $rad["klassenavn"];
+        $studiumkode = $rad["studiumkode"];
+
+        print("<tr><td>$klassekode</td><td>$klassenavn</td><td>$studiumkode</td></tr>");
+    }
+
+    print("</table>");
+}
+?>
