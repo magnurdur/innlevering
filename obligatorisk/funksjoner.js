3f595316e7418c59bb1b500418470d875300f33d
magnurdur <trevlandmagnus@gmail.com>
2025-10-22 15:22:44 +0200

    gg

--- obligatorisk/funksjoner.js
+++ obligatorisk/funksjoner.js
@@ -1,10 +1,5 @@
-<?php  
 
-$host = getenv('DB_HOST');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
-$database = getenv('DB_DATABASE');
-
- $db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
-    
- ?>
+function bekreft()
+{
+      return confirm ("Er du sikker ?");
+} 
